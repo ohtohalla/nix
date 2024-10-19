398dd8ea670032130a2f4413c59fc517e0d11ce7
Juho Kajava <juhokajava@Juhos-MacBook-Pro.local>
2024-10-19 21:20:31 +0300

    Edited wilder conf

--- system/general/nixvim/utils/wilder.nix
+++ system/general/nixvim/utils/wilder.nix
@@ -2,5 +2,17 @@
   programs.nixvim.plugins.wilder = {
     enable = true;
     modes = [ ":" "/" "?" ];
-  };
+    renderer = ''wilder.renderer_mux({
+    [':'] = wilder.popupmenu_renderer({
+      highlighter = wilder.basic_highlighter(),
+      pumblend = 20,
+      left = {' ', wilder.popupmenu_devicons()},
+      right = {' ', wilder.popupmenu_scrollbar()},
+    }),
+    ['/'] = wilder.wildmenu_renderer({
+    highlighter = wilder.basic_highlighter(),
+    }),
+    })'';
+    };
 }
+
